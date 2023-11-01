3d0f26cbaf905c26f3c7ac2e30588da4e455a423
danielnascimentotomaz <danielsabio1000@hotmail.com>
2023-11-01 13:52:54 -0300

    Implementando a classe calculadora

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Calculadora.Service;
+
+CalculadoraImpl calculadora = new CalculadoraImpl();
+
+int x = 10;
+
+int y = 5;
+
+Console.WriteLine($"{x} + {y} = {calculadora.Somar(x,y)}");
+
---
+++ Calculadora/Service/CalculadoraImpl.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculadora.Service
+{
+    public class CalculadoraImpl
+    {
+        public int Somar(int x,int y){
+            return x + y;
+        }
+    }
+}
